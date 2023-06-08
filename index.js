c93bcea798558f121240297e1632230b2926f693
Deepak Chauhan <deepak1110chauhan@gmail.com>
2023-06-08 15:18:58 +0530

    tailwind project

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('#menu');
+
+hamburger.addEventListener('click', ()=>{
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }
+    else{
+        menu.classList.add('hidden');
+    }
+});
